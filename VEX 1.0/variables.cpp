2e16d92f02735da9fd43dd6a2e28f579dce8d822
rNuv <36581026+rNuv@users.noreply.github.com>
2019-08-06 18:32:16 -0400

    Update variables.cpp

--- VEX 1.0/variables.cpp
+++ VEX 1.0/variables.cpp
@@ -2,21 +2,23 @@
 #include "main.h"
 #include <math.h>
 
-pros::Motor move1 (1);
+pros::Motor move1 (4);
 pros::Motor move2 (2);
-pros::Motor move3 (3);
-pros::Motor move4 (4);
+pros::Motor move3 (1);
+pros::Motor move4 (-3);
+
 pros::Motor twoBar (5);
 pros::Motor leftRoller (6);
 pros::Motor rightRoller (7);
+pros::Motor pusher (8);
 
 
 void drive(int ch1, int ch2, int ch3, int ch4)
 {
+  move4.move(ch3  + ch4 + ch1);
   move1.move(ch3 - ch4 + ch1);
   move2.move(-(ch3 - ch4 - ch1));
   move3.move(-(ch3 + ch4 - ch2));
-  move4.move(ch3  + ch4 + ch1);
 }
 
 void lift(bool liftUpButton, bool liftDownButton)
@@ -50,5 +52,19 @@ void roll(bool rollUpButton, bool rollDownButton)
   {
     leftRoller.move_velocity(0);
     rightRoller.move_velocity(0);
+  }
+}
+void push(bool pushForwardButton, bool pullBackwardsButton){
+  if(pushForwardButton)
+  {
+    pusher.move_velocity(20);
+  }
+  else if(pullBackwardsButton)
+  {
+    pusher.move_velocity(-20);
+  }
+  else
+  {
+    pusher.move_velocity(0);
   }
 }
