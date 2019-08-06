f29153b5c6c96bc8a24779d34559db05aa32c4e0
rNuv <36581026+rNuv@users.noreply.github.com>
2019-08-06 18:30:16 -0400

    Arnavs Basement Code

--- opcontrol.cpp
+++ opcontrol.cpp
@@ -1,48 +1,85 @@
 #include "main.h"
+#include "variables.h"
+//using namespace okapi;
 
-/**
- * Runs the operator control code. This function will be started in its own task
- * with the default priority and stack size whenever the robot is enabled via
- * the Field Management System or the VEX Competition Switch in the operator
- * control mode.
- *
- * If no competition control is connected, this function will run immediately
- * following initialize().
- *
- * If the robot is disabled or communications is lost, the
- * operator control task will be stopped. Re-enabling the robot will restart the
- * task, not resume it from where it left off.
- */
+int FRONT_LEFT = 4;
+int FRONT_RIGHT = 2;
+int BACK_LEFT = 1;
+int BACK_RIGHT = -3;
 
-using namespace okapi;
+int TWO_BAR = 5;
+int RIGHT_ROLLER = 6;
+int LEFT_ROLLER = 7;
+int PUSHER = 8;
 
-int FRONT_LEFT = 1;
-int FRONT_RIGHT = -2;
-int BACK_LEFT = -3;
-int BACK_RIGHT = 4;
+pros::Motor FrontRight (FRONT_RIGHT);
+pros::Motor FrontLeft (FRONT_LEFT);
+pros::Motor BackLeft (BACK_LEFT);
+pros::Motor BackRight (BACK_RIGHT);
+pros::Motor TwoBar (TWO_BAR);
+pros::Motor RightRoller (RIGHT_ROLLER);
+pros::Motor LeftRoller (LEFT_ROLLER);
+pros::Motor Pusher (PUSHER);
 
-auto myChassis = ChassisControllerFactory::create(
-  FRONT_LEFT, FRONT_RIGHT, BACK_LEFT, BACK_RIGHT,
-  AbstractMotor::gearset::green,
-  {4_in, 11_in}
-);
+int RightX;
+int RightY;
+int LeftY;
+int LeftX;
+bool liftUpButton;
+bool liftDownButton;
+bool pushForwardButton;
+bool pullBackwardsButton;
+int thresh = 20;
 
 void opcontrol() {
 
-	Controller controller;
-	ControllerButton runAutoButton(ControllerDigital::X);
+	pros::Controller controller (CONTROLLER_MASTER);
 
 	while(true){
-		myChassis.arcade(controller.getAnalog(ControllerAnalog::leftY), controller.getAnalog(ControllerAnalog::rightX));
 
-		if(runAutoButton.changedToPressed()){
-			for(int i = 0; i < 4; i++){
-				myChassis.moveDistance(12_in);
-				myChassis.turnAngle(90_deg);
+    RightX = controller.get_analog(ANALOG_RIGHT_X);
+		RightY = controller.get_analog(ANALOG_RIGHT_Y);
+		LeftY = controller.get_analog(ANALOG_LEFT_Y);
+		LeftX = controller.get_analog(ANALOG_LEFT_X);
+
+		bool rollUpButton = controller.get_digital(DIGITAL_R1);
+		bool rollDownButton = controller.get_digital(DIGITAL_R2);
+    bool liftUpButton = controller.get_digital(DIGITAL_L1);
+    bool liftDownButton = controller.get_digital(DIGITAL_L2);
+		bool pushForwardButton = controller.get_digital(DIGITAL_UP);
+		bool pullBackwardsButton = controller.get_digital(DIGITAL_DOWN);
+
+		if((abs(RightX) > 0) || (abs(RightY) > 0) || (abs(LeftY) > 0) || (abs(LeftX) > 0)){
+			if(abs(RightX) < thresh) {
+				RightX = 0;
+			}
+			if(abs(RightY) < thresh) {
+				RightY = 0;
+			}
+			if(abs(LeftY) < thresh) {
+				LeftY = 0;
+			}
+			if(abs(LeftX) < thresh) {
+				LeftX = 0;
+			}
+
+			if(abs(LeftY) > abs(RightY)) {
+				RightY = 0;
+			}
+			else {
+				LeftY = 0;
+			}
+
+			if(abs(LeftY) > 80){
+				RightX *= .707106781186548;
 			}
 		}
 
+		drive(RightX, RightY, LeftY, LeftX);
+    lift(liftUpButton, liftDownButton);
+		roll(rollUpButton, rollDownButton);
+		push(pushForwardButton, pullBackwardsButton);
+
 		pros::Task::delay(10);
 	}
-
 }
